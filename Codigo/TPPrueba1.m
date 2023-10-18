9118654c952f68db4363a304d9dcbe7ea8bea95b
Solana Castillo <84191129+solanabanana@users.noreply.github.com>
2023-10-18 15:54:11 -0300

    Update TPPrueba1.m

--- Codigo/TPPrueba1.m
+++ Codigo/TPPrueba1.m
@@ -7,9 +7,37 @@ end
 
 figure(1)
 imshow(img);
-imcontrast;
+% imcontrast;
 
-umbrales = multithresh(img, 2);
-img_out = imadjust(img, [92, 114]/255, [0, 255]/255);
+T = adaptthresh(img, 0.4);
+BW = imbinarize(img, T);
+
+[B, L] = bwboundaries(BW, 'noholes');
+stats = regionprops(BW, 'Area','Image');
+[area_max, i_max] = max([stats.Area]);
+imagen = stats(i_max).Image;
+
+% img_out = imadjust(img, [92, 114]/255, [0, 255]/255);
 figure(2);
-imshow(img_out);
+colors=['b' 'g' 'r' 'c' 'm' 'y'];
+imshow(BW);
+hold on;
+boundary = B{i_max};
+cidx = mod(k,length(colors))+1;
+  plot(boundary(:,2), boundary(:,1),...
+       colors(cidx),'LineWidth',2);
+
+SE2 = strel('square', 4);
+SE3 = strel('disk', 4);
+SE4 = strel('disk', 7);
+SE5 = strel('disk', 10);
+
+BW1 = imerode(imagen, SE2); 
+BW1 = imerode(BW1, SE2); 
+BW1 = imerode(BW1, SE2); 
+BW1 = imclose(BW1, SE3); 
+BW1 = imopen(BW1, SE4);
+BW1 = imdilate(BW1, SE5);
+
+figure(3)
+imshowpair(imagen, BW1, 'montage');
