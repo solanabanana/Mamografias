e6b70848d808e486bffce4a35d692c6a7dd7f0ce
Solana Castillo <84191129+solanabanana@users.noreply.github.com>
2023-11-12 18:47:54 -0300

    Update TP_Final.m

--- Codigo/TP_Final.m
+++ Codigo/TP_Final.m
@@ -1,30 +1,43 @@
-
 clc
 clear all
-
-filename = { 'M1.jpg', 'M2.jpeg', 'M3.jpeg', 'M4.jpg' ,'M6.jpeg', 'M7.jpg','M8.jpg', 'M9.jpg' ,'M10.jpg', 'M11.jpg' ,'B1.jpg' ,'B2.jpg', 'B4.jpg','B5.jpg' ,'B6.jpg', 'B7.jpg' ,'B8.jpg', 'B9.jpg', 'B10.png', 'B11.jpg'};
+%% Verificamos que la función MalignoBenigno esté clasificando bien
+filenameN = {'N1.jpg','N2.jpg', 'N3.jpg', 'N4.jpg' ,'N5.jpg'};
+for k = 1:5
+    
+    img = imread(filenameN{k}); 
+    
+    %Segmentamos el tumor 
+    tumor = TumorMama(filenameN{k});
+    
+    %Obtenemos los parámetros de la masa
+    [R, X] = MalignoBenigno(tumor, img);  
+    Normales(k)= R;
+end  
+%% Obtenemos un modelo de regresión logística
+%Utilizamos 10 imágenes de tumor malingo y 10 de benigno. Tomamos como
+%parámetros los que nos deveulve la función MalignoBenigno
+filename = {'M1.jpg','M2.jpeg', 'M3.jpeg', 'M4.jpg' ,'M6.jpeg', 'M7.jpg','M8.jpg', 'M9.jpg' ,'M10.jpg', 'M11.jpg' ,'B1.jpg' ,'B2.jpg', 'B4.jpg','B5.jpg','B6.jpg', 'B7.jpg' ,'B8.jpg', 'B9.jpg', 'B10.png', 'B11.jpg'};
 filename=filename';
 
 for k = 1:20
     
     img = imread(filename{k}); 
     
-    %segmentamos el tumor 
+    %Segmentamos el tumor 
     tumor = TumorMama(filename{k});
     
-    %determinamos si es maligno o benigno
+    %Obtenemos los parámetros de la masa
     [R, X] = MalignoBenigno(tumor, img);  
     Xt(k,:)= X;
 end  
 
 Y = [1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0];
 
-modelo = fitglm(Xt,Y,'Distribution', 'binomial', 'Link','logit');
+modelo = fitglm(Xt,Y,'Distribution', 'binomial', 'Link','logit')
+%% Verificamos el funcionamiento de nuestro modelo
+filenameV = { 'M12.jpeg', 'M13.jpeg', 'M14.jpg', 'M15.jpg' ,'M16.jpg', 'B12.jpg','B13.jpg', 'B14.png', 'B15.jpg', 'B16.jpg', 'B17.jpg'};
 
-%% 
-filenameV = { 'M12.jpeg', 'M13.jpeg', 'M14.jpeg', 'M15.jpg' ,'M16.jpg', 'B12.jpg','B13.jpg'};
-
-for k = 1:7
+for k = 1:11
     
     imgV = imread(filenameV{k}); 
     
@@ -37,6 +50,3 @@ for k = 1:7
 end
 
 y_pred = predict(modelo, Xv);
-
-% accuracy = sum(y == (y_pred > 0.5)) / length(y);
-% disp(['Accuracy: ', num2str(accuracy)]);
